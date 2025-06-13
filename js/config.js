6e1f01fd9c51f0a6bcbab4e2c77091b30cc6ca14
julio1703 <julio.elias22@gmail.com>
2025-06-13 16:46:38 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_FXZ8mSrgx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '57ht2l4ldun27a001j3vqvsutu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
